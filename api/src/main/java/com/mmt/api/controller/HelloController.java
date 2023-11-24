fd3158e5f01d582172ce411b5a3a4ba9fb912365
Lee Soyeon <data.sy.2@gmail.com>
2023-11-24 12:48:59 +0900

    api 서버 테스트

---
+++ api/src/main/java/com/mmt/api/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.mmt.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello");
+    }
+
+    @GetMapping("/image")
+    public ResponseEntity<String> imageTest(){
+        return ResponseEntity.ok("<a href=\"https://ibb.co/WD0RmTC\"><img src=\"https://i.ibb.co/KND8T1n/dog.jpg\" alt=\"dog\" border=\"0\" /></a>");
+    }
+
+}
