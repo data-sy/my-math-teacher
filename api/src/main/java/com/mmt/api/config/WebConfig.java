1e1c9a9651b072dfcf16a81f09e0c4f937937909
Lee Soyeon <data.sy.2@gmail.com>
2023-12-14 19:51:29 +0900

    cors에 뷰서버 5173 추가

--- api/src/main/java/com/mmt/api/config/WebConfig.java
+++ api/src/main/java/com/mmt/api/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("/*") // 외부에서 들어오는 모든 url 허용
                 .allowedOrigins("http://localhost:8080", "http://15.164.232.32:8080",
                         "http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.2.17:3000",
-                        "http://localhost:8000", "http://3.35.209.39:8000"
+                        "http://localhost:8000", "http://3.35.209.39:8000",
+                        "http://localhost:5173"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
