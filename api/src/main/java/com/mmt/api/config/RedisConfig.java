1a291af38d955601d4b0552849646f8fb62fbfc1
Lee Soyeon <data.sy.2@gmail.com>
2024-02-09 20:18:34 +0900

    Redis DB 비밀번호 추가

--- api/src/main/java/com/mmt/api/config/RedisConfig.java
+++ api/src/main/java/com/mmt/api/config/RedisConfig.java
@@ -1,10 +1,12 @@
 package com.mmt.api.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -16,12 +18,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    private final RedisProperties redisProperties;
+//    private final RedisProperties redisProperties;
 
-    // RedisProperties로 yaml에 저장한 host, post를 연결
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+        // System.out.println("redis host" + host);
+        // System.out.println("redis port" + port);
+        // System.out.println("redis password" + password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
--- api/src/main/java/com/mmt/api/config/SecurityConfig.java
+++ api/src/main/java/com/mmt/api/config/SecurityConfig.java
@@ -75,8 +75,8 @@ public class SecurityConfig {
 //                                // 개발을 위해 우선 열어두자
 //                                .requestMatchers("/api/v1/**").permitAll()
                                 // 인증 없이 접근 가능
-                                .requestMatchers("/", "/favicon.ico", "/api/v1/hello/**", "/api/v1/signup", "/api/v1/authentication", "/api/v1/checkDuplicate", "/api/v1/reissue", "/api/v1/login").permitAll()
-                                .requestMatchers("/api/v1/chapters/**", "/api/v1/concepts/**", "/api/v1/tests/school-level/**", "/api/v1/tests/detail/**").permitAll()
+                                .requestMatchers("/", "/favicon.ico", "/api/v1/hello/**").permitAll()
+                                .requestMatchers("api/v1/auth/**", "/api/v1/chapters/**", "/api/v1/concepts/**", "/api/v1/tests/school-level/**", "/api/v1/tests/detail/**").permitAll()
                                 .requestMatchers("/login.html", "/oauth2/**").permitAll()
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasRole('DBA')"))
