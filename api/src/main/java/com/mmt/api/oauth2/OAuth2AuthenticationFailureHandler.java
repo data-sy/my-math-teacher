3b8f7b1193e27ad3331c8063d0eb0f31cbb371e4
Lee Soyeon <data.sy.2@gmail.com>
2024-06-21 08:45:36 +0900

    FailureHandler에 디버깅 추가

--- api/src/main/java/com/mmt/api/oauth2/OAuth2AuthenticationFailureHandler.java
+++ api/src/main/java/com/mmt/api/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -3,13 +3,17 @@ package com.mmt.api.oauth2;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
@@ -17,6 +21,16 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException {
         String targetUrl = "/";
+        String errorMessage = authenticationException.getLocalizedMessage();
+
+        // Log detailed information
+        log.error("Authentication failed: {}", errorMessage);
+
+        if (authenticationException instanceof OAuth2AuthenticationException) {
+            OAuth2Error error = ((OAuth2AuthenticationException) authenticationException).getError();
+            log.error("OAuth2 error: {}", error.toString());
+        }
+
 
         targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
                 .queryParam("error", authenticationException.getLocalizedMessage())
