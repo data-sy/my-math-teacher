eded7bd73f5e6de0033129164ef801f54b8c6a96
Lee Soyeon <data.sy.2@gmail.com>
2024-06-21 15:22:07 +0900

    로그 심기

--- api/src/main/java/com/mmt/api/oauth2/OAuth2UserInfoFactory.java
+++ api/src/main/java/com/mmt/api/oauth2/OAuth2UserInfoFactory.java
@@ -1,9 +1,13 @@
 package com.mmt.api.oauth2;
 
+import lombok.extern.slf4j.Slf4j;
 import java.util.Map;
 
+@Slf4j
 public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(AuthProvider authProvider, Map<String, Object> attributes) {
+        log.debug("Creating OAuth2UserInfo for provider: {}", authProvider);
+        log.debug("Attributes received: {}", attributes);
         switch (authProvider) {
             case GOOGLE: return new GoogleOAuth2User(attributes);
             case NAVER: return new NaverOAuth2User(attributes);
