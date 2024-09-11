48e8a08b4dd4efd2261cc268132d87a3064fd884
Lee Soyeon <data.sy.2@gmail.com>
2024-09-12 07:47:05 +0900

    fix: request 조건 수정
    - GET 요청이므로 body말고 param으로

--- api/src/main/java/com/mmt/api/controller/ItemController.java
+++ api/src/main/java/com/mmt/api/controller/ItemController.java
@@ -1,11 +1,10 @@
 package com.mmt.api.controller;
 
-import com.mmt.api.dto.item.PersonalItemsRequest;
 import com.mmt.api.dto.item.PersonalItemsResponse;
 import com.mmt.api.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,8 +23,8 @@ public class ItemController {
      * 맞춤학습지 출제 문항 목록
      */
     @GetMapping("/personal")
-    public List<PersonalItemsResponse> getPersonalItems(@RequestBody PersonalItemsRequest request){
-        return itemService.findPersonalItems(request);
+    public List<PersonalItemsResponse> getPersonalItems(@RequestParam("userTestId") Long userTestId){
+        return itemService.findPersonalItems(userTestId);
     }
 
     // 아이템 하나 바꾸는 건 아이템 컨트롤러로 추가
