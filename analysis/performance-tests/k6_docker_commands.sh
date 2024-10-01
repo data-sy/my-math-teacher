f8ae814c0f23f693fd8f5ac18802d01e9e48b892
Lee Soyeon <data.sy.2@gmail.com>
2024-10-01 16:03:34 +0900

    feat: k6 성능테스트
    - concept_id에 따른 문항 중 하나 랜덤으로 추출하는 api
    - 기존 쿼리 : sql에서 order by 사용
    - api1 : order by 부분을 자바단에서 해결
    - api2 : 자바에서 미리 랜덤하게 숫자 뽑아서 해당 item_id로 쿼리
    - api3 : 미리 랜덤하게 숫자 뽑는 부분을 MySQL에서 => 즉, 오더바이 절을 인라인 뷰로

--- analysis/performance-tests/k6_docker_commands.sh
+++ analysis/performance-tests/k6_docker_commands.sh
@@ -4,3 +4,5 @@ docker pull grafana/k6
 # 컨테이너 실행
 docker run -v $(pwd):/scripts -i grafana/k6 run /scripts/performance_test.js
 docker run --rm -v $(pwd):/scripts -v $(pwd)/results:/results -i grafana/k6 run /scripts/test_concepts_by_chapter.js
+
+docker run --rm -v $(pwd):/scripts -i grafana/k6 run /scripts/test_item_by_concept.js
---
+++ analysis/performance-tests/test_item_by_concept.js
@@ -0,0 +1,44 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 500 }, // 1분 동안 사용자 n명까지 증가
+        { duration: '3m', target: 500 }, // 3분 동안 n명의 사용자 유지
+        { duration: '1m', target: 0 },   // 1분 동안 사용자 0명으로 감소
+    ],
+};
+
+export default function () {
+  let conceptId = 1009;
+
+//   // 기존 API 테스트
+//   let res0 = http.get(`http://host.docker.internal:8080/api/v1/perf-test/originalQuery?conceptId=${conceptId}`);
+//   check(res0, {
+//       'API 0: status was 200': (r) => r.status === 200,
+//       'API 0: response time < 500ms': (r) => r.timings.duration < 500,
+//   });
+
+//   // 개선1 API 테스트
+//   let res1 = http.get(`http://host.docker.internal:8080/api/v1/perf-test/javaSort?conceptId=${conceptId}`);
+//   check(res1, {
+//       'API 1: status was 200': (r) => r.status === 200,
+//       'API 1: response time < 500ms': (r) => r.timings.duration < 500,
+//   });
+
+//   // 개선2 API 테스트
+//   let res2 = http.get(`http://host.docker.internal:8080/api/v1/perf-test/javaRandomFetch?conceptId=${conceptId}`);
+//   check(res2, {
+//       'API 2: status was 200': (r) => r.status === 200,
+//       'API 2: response time < 500ms': (r) => r.timings.duration < 500,
+//   });
+
+  // 개선3 API 테스트
+  let res3 = http.get(`http://host.docker.internal:8080/api/v1/perf-test/dbOptimized?conceptId=${conceptId}`);
+  check(res3, {
+      'API 3: status was 200': (r) => r.status === 200,
+      'API 3: response time < 500ms': (r) => r.timings.duration < 500,
+  });
+
+}
+
