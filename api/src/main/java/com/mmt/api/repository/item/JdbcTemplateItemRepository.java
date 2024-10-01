30965ab200ee35bc04f1c0f2f887083a89068996
Lee Soyeon <data.sy.2@gmail.com>
2024-10-01 15:57:12 +0900

    style: sql 문자열 안의 세미콜론 삭제

--- api/src/main/java/com/mmt/api/repository/item/JdbcTemplateItemRepository.java
+++ api/src/main/java/com/mmt/api/repository/item/JdbcTemplateItemRepository.java
@@ -19,7 +19,7 @@ public class JdbcTemplateItemRepository implements ItemRepository{
     @Override
     public Item findByConceptId(int conceptId) {
         String sql = "SELECT i.item_id, i.item_answer, i.item_image_path, c.concept_name, ch.school_level, ch.grade_level, ch.semester FROM items i \n" +
-                "JOIN concepts c ON i.concept_id = c.concept_id JOIN chapters ch ON c.concept_chapter_id = ch.chapter_id WHERE c.concept_id = ? ORDER BY RAND() LIMIT 1;";
+                "JOIN concepts c ON i.concept_id = c.concept_id JOIN chapters ch ON c.concept_chapter_id = ch.chapter_id WHERE c.concept_id = ? ORDER BY RAND() LIMIT 1";
         return jdbcTemplate.queryForObject(sql, itemRowMapper(), conceptId);
     }
 
