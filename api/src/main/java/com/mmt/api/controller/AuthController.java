7fa4abe27bc1fa517f3377444e17046f2508d48b
Lee Soyeon <data.sy.2@gmail.com>
2024-10-23 14:23:20 +0900

     remove: auth api에서 사용하지 않는 것들 삭제

--- api/src/main/java/com/mmt/api/controller/AuthController.java
+++ api/src/main/java/com/mmt/api/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
     }
 
     /**
-     * 유저 회원가입
+     * 회원가입
      */
     @PostMapping("/signup")
     public ResponseEntity<UserDTO> signup(@Valid @RequestBody UserDTO userDTO) {
@@ -35,7 +35,16 @@ public class AuthController {
     }
 
     /**
-     * login
+     * 아이디 중복 검사
+     */
+    @GetMapping("/duplication")
+    public ResponseEntity<Boolean> checkDuplicate(@RequestParam String userEmail) {
+        boolean isDuplicate = userService.isDuplicateUser(userEmail);
+        return ResponseEntity.ok(isDuplicate);
+    }
+
+    /**
+     * 로그인
      * 수정할 것 : 로그인DTO를 loginRequest로 바꾸고 변수명도 request로 바꾸기
      * 로그인 한 회원의 id도 꺼내서 토큰과 같이 담아서 loginResponse 만들기
      * 아! 그러면 토큰 DTO 말고 그냥 토큰 자체 담아도 될 듯!
@@ -52,52 +61,7 @@ public class AuthController {
     }
 
     /**
-     * 아이디 중복 검사
-     */
-    @GetMapping("/duplication")
-    public ResponseEntity<Boolean> checkDuplicate(@RequestParam String userEmail) {
-        boolean isDuplicate = userService.isDuplicateUser(userEmail);
-        return ResponseEntity.ok(isDuplicate);
-    }
-
-    /**
-     * 현재 비밀번호 확인 (개인정보 수정 시)
-     * Q. 필드가 같아서 LoginDTO 재활용 했는데, validateRequest DTO를 따로 만들어주는 게 좋을까? 내용이 같더라도?
-     */
-    @PostMapping("/validation")
-    public ResponseEntity<Boolean> validateUser(@RequestBody LoginDTO loginDTO) {
-        boolean isValid = authService.validateCurrentPassword(loginDTO.getUserEmail(), loginDTO.getUserPassword());
-        System.out.println(isValid);
-        return ResponseEntity.ok(isValid);
-    }
-
-//    /**
-//     * oauth login
-//     */
-//    @GetMapping("/login")
-//    public ResponseEntity<?> login(@RequestParam("token") String token) {
-//        if (token != null && !token.isEmpty()) {
-//            // accessToken과 refreshToken 추출
-//            String accessToken = extractTokenValue(token, "accessToken");
-//            String refreshToken = extractTokenValue(token, "refreshToken");
-//
-//            // 추출한 토큰 값을 출력하거나 다른 작업 수행
-//            System.out.println("AccessToken: " + accessToken);
-//            System.out.println("RefreshToken: " + refreshToken);
-//
-//            JwtToken jwtToken = new JwtToken("Bearer", accessToken, refreshToken);
-//
-//            // 여기서 추출한 accessToken과 refreshToken을 사용하여 다른 엔드포인트로 보낼 수 있어요.
-//             sendTokenToAnotherEndpoint(jwtToken);
-//
-//            return ResponseEntity.ok("Token received and extracted.");
-//        } else {
-//            return ResponseEntity.badRequest().body("Token not found in query string.");
-//        }
-//    }
-
-    /**
-     * AccessToken이 만료되었을 때 토큰(AccessToken , RefreshToken)재발급
+     * AccessToken이 만료 되었을 때 토큰(AccessToken , RefreshToken)재발급
      */
     @PostMapping("/reissue")
     public ResponseEntity<TokenDTO> reissue(@Valid @RequestBody TokenDTO request) {
@@ -123,36 +87,15 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
-//    private String extractTokenValue(String token, String tokenType) {
-//        String pattern = tokenType + "=([^,&)]+)";
-//        Pattern regex = Pattern.compile(pattern);
-//        Matcher matcher = regex.matcher(token);
-//
-//        if (matcher.find()) {
-//            return matcher.group(1);
-//        }
-//        return null;
-//    }
-//
-//    private void sendTokenToAnotherEndpoint(JwtToken jwtToken) {
-//        // 여기서 token을 새 엔드포인트로 전송하는 로직을 구현
-//        // HTTP 요청을 보내거나 다른 서비스로 전달할 수 있습니다.
-//        // 예를 들어 RestTemplate, HttpClient 등을 사용하여 POST 요청을 보낼 수 있습니다.
-//        // 아래는 예시 코드입니다.
-//
-//        String targetEndpoint = "http://localhost:5173/login"; // 바디에 토큰을 담아서 보낼 엔드포인트
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwtToken);
-//
-//        HttpEntity<JwtToken> requestEntity = new HttpEntity<>(jwtToken, headers);
-//
-//        // 여기서 Connection refused 에러가 뜸
-//        ResponseEntity<String> response = restTemplate.exchange(targetEndpoint, HttpMethod.GET, requestEntity, String.class);
-//
-//        // 만약 다른 엔드포인트로의 응답을 처리해야 한다면 여기서 처리할 수 있습니다.
-//        // response.getBody() 등을 통해 응답 내용을 확인할 수 있습니다.
-//    }
+    /**
+     * 현재 비밀번호 확인 (개인정보 수정 시)
+     * Q. 필드가 같아서 LoginDTO 재활용 했는데, validateRequest DTO를 따로 만들어주는 게 좋을까? 내용이 같더라도?
+     */
+    @PostMapping("/validation")
+    public ResponseEntity<Boolean> validateUser(@RequestBody LoginDTO loginDTO) {
+        boolean isValid = authService.validateCurrentPassword(loginDTO.getUserEmail(), loginDTO.getUserPassword());
+        System.out.println(isValid);
+        return ResponseEntity.ok(isValid);
+    }
+
 }
